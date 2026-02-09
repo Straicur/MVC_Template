638acfb490095093e2cac03996bd84cdca2d635c
damian-mosinski <mosinskidamian11@gmail.com>
2026-02-09 17:32:11 +0100

    Poprawa TestControllera

--- src/Controller/Api/TestController.php
+++ src/Controller/Api/TestController.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace App\Controller\Api;
 
 use App\DTO\Request\TestRequestDTO;
+use App\DTO\Response\TestResponseDTO;
 use App\ExceptionManagement\Exceptions\ApiException\BadRequestException\BadRequestException;
 use App\ExceptionManagement\Exceptions\ApiException\BadRequestException\BadRequestExceptionModel;
 use App\ExceptionManagement\Exceptions\ApiException\UnauthorizedException\UnauthorizedException;
@@ -19,21 +20,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[OA\Response(
-    response   : 400,
+    response: 400,
     description: 'JSON Data Invalid',
-    content    : new Model(type: BadRequestExceptionModel::class)
+    content: new Model(type: BadRequestExceptionModel::class)
 )]
 #[OA\Response(
-    response   : 401,
+    response: 401,
     description: 'User not authorized',
-    content    : new Model(type: UnauthorizedExceptionModel::class)
+    content: new Model(type: UnauthorizedExceptionModel::class)
 )]
 #[OA\Response(
-    response   : 422,
+    response: 422,
     description: 'Unprocessable Content',
-    content    : new Model(type: UnprocessableContentExceptionModel::class)
+    content: new Model(type: UnprocessableContentExceptionModel::class)
 )]
 #[OA\Tag(name: 'Test')]
 final class TestController extends AbstractController
@@ -41,12 +45,14 @@ final class TestController extends AbstractController
     public function __construct(
         private readonly RequestServiceInterface $requestService,
         private readonly AuthServiceInterface $authService,
+        private readonly SerializerInterface $serializer,
     ) {}
 
     /**
      * @throws BadRequestException
      * @throws UnauthorizedException
      * @throws UnprocessableContentException
+     * @throws ExceptionInterface
      */
     #[Route('/api/test', name: 'test', methods: ['POST'])]
     #[OA\Post(
@@ -62,6 +68,7 @@ final class TestController extends AbstractController
             new OA\Response(
                 response: 200,
                 description: 'Success',
+                content: new Model(type: TestResponseDTO::class),
             ),
         ]
     )]
@@ -76,6 +83,10 @@ final class TestController extends AbstractController
 
         $user = $this->authService->getUserFromAccessToken();
 
-        return new Response($user->getEmail(), status: Response::HTTP_OK);
+        $response = new TestRequestDTO(
+            email: $user->getEmail(),
+        );
+
+        return new Response($this->serializer->serialize(data: $response, format: JsonEncoder::FORMAT), status: Response::HTTP_OK);
     }
 }
